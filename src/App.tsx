a4f0a67f28539b5bb18852e0ab6f74a87672ff8e
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-05-30 19:12:14 +0200

    changed some styles and added the board

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,15 @@
-import { useState } from "react";
-import logo from "resources/icons/logo.svg";
 import "styles/App.scss";
+import Board from "components/Board";
 
 const App = (): JSX.Element => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <header>
+        <h1>Snake</h1>
       </header>
+      <main>
+        <Board />
+      </main>
     </div>
   );
 };
