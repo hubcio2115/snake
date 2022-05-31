c1893f19e3f5cb4691961cc80859d2c0c5966cd7
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-05-31 16:28:43 +0200

    added tests for each utility function

---
+++ test/getDirectionFromKey.test.ts
@@ -0,0 +1,33 @@
+import {getDirectionFromKey} from '../src/utils/lib';
+
+describe('returns correct direction after getting key from an keydown event', () => {
+  it('returns "UP", given key = ArrowUp', () => {
+    const direction = getDirectionFromKey('ArrowUp');
+
+    expect(direction).toBe('UP');
+  });
+
+  it('returns "DOWN", given key = ArrowDown', () => {
+    const direction = getDirectionFromKey('ArrowDown');
+
+    expect(direction).toBe('DOWN');
+  });
+
+  it('returns "LEFT", given key = ArrowLeft', () => {
+    const direction = getDirectionFromKey('ArrowLeft');
+
+    expect(direction).toBe('LEFT');
+  });
+
+  it('returns "RIGHT", given key = ArrowRight ', () => {
+    const direction = getDirectionFromKey('ArrowRight');
+
+    expect(direction).toBe('RIGHT');
+  });
+
+  it('returns "", given a key that is not in DIRECTIONS enum', () => {
+    const direction = getDirectionFromKey('A');
+
+    expect(direction).toBe('');
+  });
+});
