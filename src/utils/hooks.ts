c74b14201615b17e2861479828ac70b4eda97d52
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-06-02 22:23:17 +0200

    docs: added source of useInterval

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,5 +1,8 @@
 import { useEffect, useRef } from 'react';
 
+// I found this hook in this article:
+// https://overreacted.io/making-setinterval-declarative-with-react-hooks/#extracting-a-hook
+
 export function useInterval(callback: Function, delay: number) {
   const savedCallback = useRef<Function>();
 
