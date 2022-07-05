7afecf9ad212a1d7482482b1f900b8c2d8efc16a
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-07-05 14:49:49 +0200

    fix: typo

--- src/views/StartingScreenView/StartingScreenView.tsx
+++ src/views/StartingScreenView/StartingScreenView.tsx
@@ -10,7 +10,7 @@ interface StartingScreenViewProps {
 
 const StartingScreenView = ({
   isLeaderBoardLoading,
-}: StartingScreenViewProps) => {
+}: StartingScreenViewProps): JSX.Element => {
   const navigate = useNavigate();
 
   return (
